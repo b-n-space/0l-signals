12e886357721ad90727954df091c13913f49a16d
N Space <1257310+nourspace@users.noreply.github.com>
2023-04-11 00:29:09 +0200

    fix: fetching proposals
    https://stackoverflow.com/questions/58431524/why-axios-call-my-api-in-http-when-i-use-a-https-adress
    
    Removed unused chart.js from index.js

--- web/index.js
+++ web/index.js
@@ -1,13 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-import {
-  Chart,
-  registerables,
-} from 'https://cdn.jsdelivr.net/npm/chart.js@4.2.1/+esm'
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.4/+esm'
 import { BnCard, BnFooter, BnHero, BnNav } from './components/index.js'
 
-// https://github.com/sgratzl/chartjs-chart-wordcloud/issues/4#issuecomment-827304369
-Chart.register(...registerables)
 
 createApp({
   components: {
@@ -21,7 +15,7 @@ createApp({
   computed: {},
   methods: {},
   async mounted () {
-    const { data } = await axios.get('/api/proposals/')
+    const { data } = await axios.get('/api/proposals')
     this.proposals = data.map(
       p => ({
         title: p.title,
